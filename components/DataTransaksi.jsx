f6e2a311efd6dab0abb7f9d20b2407bc2aa0c141
CoreSinText <coresintext@gmail.com>
2023-01-16 04:42:35 +0700

    update

--- components/DataTransaksi.jsx
+++ components/DataTransaksi.jsx
@@ -1,6 +1,20 @@
+import axios from "axios";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function DataTransaksi() {
+    const [dataPenjualan, setdataPenjualan] = useState([]);
+    useEffect(() => {
+        async function fetchApi() {
+            const req = await axios({
+                method: "get",
+                url: "http://localhost:8000/transaksi"
+            })
+
+            setdataPenjualan(req.data)
+        }
+        fetchApi()
+    }, [])
     return (
         <div className="w-full px-10 pt-24">
             <h1 className="text-2xl font-bold">Data Transaksi</h1>
@@ -18,24 +32,33 @@ export default function DataTransaksi() {
                         <th className="bg-[#285430] text-white">Total</th>
                         <th className="bg-[#285430] text-white">Tindakan</th>
                     </tr>
-                    <tr className="odd:bg-slate-200">
-                        <td>asdsa</td>
-                        <td>asdsa</td>
-                        <td>asdsa</td>
-                        <td>asdsa</td>
-                        <td>asdsa</td>
-                        <td>asdsa</td>
-                        <td>asdasm</td>
-                    </tr>
-                    <tr className="odd:bg-slate-200">
-                        <td>asdsa</td>
-                        <td>asdsa</td>
-                        <td>asdsa</td>
-                        <td>asdsa</td>
-                        <td>asdsa</td>
-                        <td>asdsa</td>
-                        <td>asdasm</td>
-                    </tr>
+
+                    {dataPenjualan.map((data, index) => {
+                        let id = data.transaksi_id
+                        let namaPembeli = data.nama_pembeli
+                        let tanggal = data.tanggal
+                        let jumlah = data.jumlah_pembelian
+                        let namaProduk = data.nama_produk
+                        let totalTransaksi = data.total_transaksi
+                        const date = new Date(tanggal)
+                        const styleDate = {
+                            weekday: "long",
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                        };
+                        return (
+                            <tr key={id} className="odd:bg-slate-200">
+                                <td className="w-24">{index + 1}</td>
+                                <td>{namaPembeli}</td>
+                                <td>{date.toLocaleDateString("id", styleDate)}</td>
+                                <td>{namaProduk}</td>
+                                <td>{jumlah}</td>
+                                <td>{totalTransaksi}</td>
+                                <td></td>
+                            </tr>
+                        )
+                    })}
                 </table>
             </div>
         </div>
