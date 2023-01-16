1cf72c83bf9bb26e2ba66867bdef88caee1a56f5
CoreSinText <coresintext@gmail.com>
2023-01-17 03:42:05 +0700

    membuat post tambah produk

--- components/TambahProduk.jsx
+++ components/TambahProduk.jsx
@@ -1,5 +1,31 @@
+import axios from "axios";
 import Link from "next/link"
+import { useState, useRef, useEffect } from "react"
 export default function TambahProduk() {
+    const [namaProduk, setnamaProduk] = useState('');
+    const [hargaBarang, sethargaBarang] = useState('');
+    const [buttonDisable, setbuttonDisable] = useState(true);
+
+
+
+    async function kirimData() {
+        await axios({
+            method: 'post',
+            url: 'http://localhost:8000/produk/tambah',
+            data: {
+                namaProduk: `${namaProduk}`,
+                harga: `${parseInt(hargaBarang)}`
+            }
+        })
+    }
+
+    useEffect(() => {
+        if (namaProduk.length > 0 && hargaBarang.length > 0) {
+            setbuttonDisable(false)
+        } else {
+            setbuttonDisable(true)
+        }
+    }, [namaProduk, hargaBarang])
     return (
         <div className="w-full px-10 pt-24">
             <h1 className="text-2xl font-bold">Tambah Transaksi</h1>
@@ -12,17 +38,17 @@ export default function TambahProduk() {
                     <form action="" className="flex flex-col p-5 space-y-5 w-full">
                         <div className="flex space-x-6 items-center w-full">
                             <h2 className="text-white w-36 ">Nama Produk</h2>
-                            <input type="text" name="" placeholder="Nama Produk" className="py-2 rounded-lg px-4 w-72" />
+                            <input value={namaProduk} onChange={e => { setnamaProduk(e.target.value) }} type="text" name="" placeholder="Nama Produk" className="py-2 rounded-lg px-4 w-72" />
                         </div>
 
 
                         <div className="flex space-x-6 items-center w-full">
                             <h2 className="text-white w-36">Nama Harga</h2>
-                            <input type="text" name="" placeholder="Harga" className="py-2 rounded-lg px-4 w-72" />
+                            <input value={hargaBarang} onChange={e => sethargaBarang(e.target.value)} type="number" name="" placeholder="Harga" className="py-2 rounded-lg px-4 w-72" />
                         </div>
 
                     </form>
-                    <button className="bg-[#68B984] px-7 py-3 w-fit rounded-lg text-white self-end mx-10">Simpan</button>
+                    <button disabled={buttonDisable} onClick={() => { kirimData() }} className={` px-7 py-3 w-fit rounded-lg text-white self-end mx-10 ${buttonDisable ? 'bg-red-400' : 'bg-[#68B984]'}`}>Simpan</button>
                 </div>
             </div>
         </div>
